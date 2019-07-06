a0abf7d6c192ca4cbf47558bcdcf5676598871d8
Nico Schlömer <nico.schloemer@gmail.com>
2019-07-06 14:17:37 +0200

    add code

---
+++ termtables/__init__.py
@@ -0,0 +1,11 @@
+from .__about__ import __author__, __email__, __license__, __status__, __version__
+from .main import get_string
+
+__all__ = [
+    "__author__",
+    "__email__",
+    "__license__",
+    "__version__",
+    "__status__",
+    "get_string",
+]
